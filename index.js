2c5e138e9ac59bcd79bb67755c5c5699afc58272
Manish <manish@radii.dev>
2023-03-28 16:31:44 +1100

    Scaffold

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const PORT = process.env.PORT || 16749;
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const listener = app.listen(PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
